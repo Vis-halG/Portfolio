bac60f1b188e04ab7bdc64a0a20ea9bd9b7bb753
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-06 15:24:22 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
               target="_blank"
               className=" hover:text-blue-700 text-[1.5rem] md:text-[1.5rem] transition-transform duration-300 transform hover:scale-125 hover:text-[#ff5f49]"
             >
-             <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-square-github"></i>
             </a>
             <a
               href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
