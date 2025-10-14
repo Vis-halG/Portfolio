e3701a77907e9640a1c05abddc5bc3935517cf31
Vis-halG <vishalgupta25980@gmail.com>
2025-10-14 09:58:29 +0530

    update

--- src/Components/CursorFollower/CursorFollower.jsx
+++ src/Components/CursorFollower/CursorFollower.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import "./cursorFollower.css";
+import "./CursorFollower.css";
 
 const CursorFollower = () => {
   const circleRef = useRef(null);
